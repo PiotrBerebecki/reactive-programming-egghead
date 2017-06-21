95e38a9821bd0d8912e39125625f94229c9aa1a5
PiotrBerebecki <petebere@gmail.com>
2017-06-21 14:15:56 +0100

    Share network requests

---
+++ 07-share-network-requests-with-merge/app.js
@@ -0,0 +1,68 @@
+const refreshBtnDOM = document.querySelector('.refresh');
+const refreshClickStream = Rx.Observable.fromEvent(refreshBtnDOM, 'click');
+
+const requestOnRefreshStream = refreshClickStream.map(ev => {
+  return 'https://api.github.com/users';
+});
+
+const startupRequestStream = Rx.Observable.of('https://api.github.com/users');
+
+const responseStream = startupRequestStream
+  .merge(requestOnRefreshStream)
+  .delay(500) // added for debugging
+  .flatMap(url => {
+    console.log('do network request');
+    return Rx.Observable.fromPromise(fetch(url));
+  })
+  .flatMap(res => Rx.Observable.fromPromise(res.json()))
+  .publishReplay(1)
+  .refCount(1);
+
+const suggestion1Stream = createSuggestionStream(responseStream);
+const suggestion2Stream = createSuggestionStream(responseStream);
+const suggestion3Stream = createSuggestionStream(responseStream);
+
+suggestion1Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion1');
+});
+
+suggestion2Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion2');
+});
+
+suggestion3Stream.subscribe(user => {
+  renderSuggestion(user, '.suggestion3');
+});
+
+// ----u-------------u-->
+//     startWith(N)
+// N---u---------------->
+// N---u---N----N------->
+//       merge
+// N---u---N----N----u-->
+
+function createSuggestionStream(responseStream) {
+  return responseStream
+    .map(userList => {
+      return userList[Math.floor(Math.random() * userList.length)];
+    })
+    .startWith(null)
+    .merge(refreshClickStream.map(ev => null));
+}
+
+function renderSuggestion(user, selector) {
+  const parentDOM = document.querySelector(selector);
+  if (user === null) {
+    // console.log(null);
+    parentDOM.style.visibility = 'hidden';
+    return;
+  }
+  parentDOM.style.visibility = 'visible';
+
+  const usernameDOM = parentDOM.querySelector('.username');
+  usernameDOM.textContent = user.login;
+  usernameDOM.href = user.html_url;
+
+  const avatarDOM = parentDOM.querySelector('img');
+  avatarDOM.src = user.avatar_url;
+}
